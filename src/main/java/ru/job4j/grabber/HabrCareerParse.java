07ef4a83ffe564730be238ce868f0457af002b9f
ivkhl <khlaponin.dp@gmail.com>
2024-06-25 12:55:58 +0200

    2.1.1 Parsing https://career.habr.com/vacancies/java_developer?page=1 [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -12,23 +12,25 @@ public class HabrCareerParse {
             https://career.habr.com""";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
+    public static final int START_PAGE_NUMBER = 1;
+    public static final int END_PAGE_NUMBER = 5;
 
     public static void main(String[] args) throws IOException {
-        int pageNumber = 1;
-        String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
-        Connection connection = Jsoup.connect(fullLink);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            Element dateElement = row.select(".vacancy-card__date").first();
-            Element timeElement = dateElement.child(0);
-            String date = timeElement.attr("datetime");
-            System.out.printf("%s %s %s%n", vacancyName, link, date);
-        });
+        for (int i = START_PAGE_NUMBER; i <= END_PAGE_NUMBER; i++) {
+            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, i, SUFFIX);
+            Connection connection = Jsoup.connect(fullLink);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                Element dateElement = row.select(".vacancy-card__date").first();
+                Element timeElement = dateElement.child(0);
+                String date = timeElement.attr("datetime");
+                System.out.printf("%s %s %s%n", vacancyName, link, date);
+            });
+        }
     }
-
 }
