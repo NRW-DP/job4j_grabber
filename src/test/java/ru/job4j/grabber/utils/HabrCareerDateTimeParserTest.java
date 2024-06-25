1f1b50215830043e861935cbd5407e8193bb2f77
ivkhl <khlaponin.dp@gmail.com>
2024-06-25 12:33:58 +0200

    2.1 Date conversion [#289476 #502142]

--- src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -2,24 +2,32 @@ package ru.job4j.grabber.utils;
 
 import org.junit.Test;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class HabrCareerDateTimeParserTest {
 
     @Test
-    public void whenParse() {
-        LocalDateTime date = LocalDateTime.now();
-        String text = date.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-        LocalDateTime parsedDate = new HabrCareerDateTimeParser().parse(text);
-        assertEquals(date, parsedDate);
-    }
-
-    @Test
-    public void whenParseEmptyString() {
+    public void whenEmptyString() {
         String emptyString = "";
         LocalDateTime parsedDate = new HabrCareerDateTimeParser().parse(emptyString);
         assertNull(parsedDate);
+    }
+
+    @Test
+    public void whenParse() {
+        String dateString = "2022-01-21T13:30:00";
+        LocalDateTime dateTime = LocalDateTime.of(2022, 1, 21, 13, 30, 0);
+        LocalDateTime rsl = new HabrCareerDateTimeParser().parse(dateString);
+        assertEquals(dateTime, rsl);
+    }
+
+    @Test
+    public void parseInvalidString() {
+        String dateString = "1020-07-33T11:85:60";
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+        LocalDateTime result = parser.parse(dateString);
+        assertNull(result);
     }
 }
